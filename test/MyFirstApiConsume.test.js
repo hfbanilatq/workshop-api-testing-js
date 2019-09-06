6c08e1f11917350ec9d0f5d5cafff7583ce5951a
hfbanilatq <hfbanilatq@gmail.com>
2019-09-06 13:13:28 -0500

    AAñadiendo analizador de codigo estatico eslint al proyecto

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -1,100 +1,99 @@
 
 const agent = require('superagent');
-const statusCode= require('http-status-codes');
+const statusCode = require('http-status-codes');
 const chai = require('chai');
 
-const expect = chai.expect;
+const { expect } = chai;
 
-describe('first api test', () =>{
+describe('first api test', () => {
+  it('Consume GET Service', async () => {
+    const response = await agent.get('https://httpbin.org/ip');
 
-    it('Consume GET Service', async () => {
-        const response = await agent.get('https://httpbin.org/ip');
-      
-        expect(response.status).to.equal(statusCode.OK);
-        expect(response.body).to.have.property('origin');
-      });
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property('origin');
+  });
 
-    it('Consume GET Service with query parameters', async() => {
-        const query = {
-            name: 'Jhon',
-            age: '31',
-            city: 'New York'
-        }
-        const response = await agent.get('https://httpbin.org/get').query(query);
+  it('Consume GET Service with query parameters', async () => {
+    const query = {
+      name: 'Jhon',
+      age: '31',
+      city: 'New York'
+    };
+    const response = await agent.get('https://httpbin.org/get').query(query);
 
-        expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.args).to.eql(query);
-    });
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.args).to.eql(query);
+  });
 
-    it('Consume HEAD service', async () =>{
-        const query = {
-            name: 'Jhon',
-            age: '31',
-            city: 'New York'
+  it('Consume HEAD service', async () => {
+    const query = {
+      name: 'Jhon',
+      age: '31',
+      city: 'New York'
 
-        }
-        const response = await agent.head('https://httpbin.org/headers').query(query);
-        const todayDate= new Date();
+    };
+    const response = await agent.head('https://httpbin.org/headers').query(query);
+    const todayDate = new Date();
 
-        expect(response.status).to.equal(statusCode.OK);
+    expect(response.status).to.equal(statusCode.OK);
 
-        const serverDate= new Date(response.header['date']);
+    const serverDate = new Date(response.header.date);
 
-        expect(response.header['content-length']).to.equal('49');
-        expect(serverDate.getDate()).to.equal(todayDate.getDate());
-        expect(response.body).to.eql({});
-    });
+    expect(response.header['content-length']).to.equal('49');
+    expect(serverDate.getDate()).to.equal(todayDate.getDate());
+    expect(response.body).to.eql({});
+  });
 
-    it('Consume PATCH service ', async () =>{
-      const query = {
-            name: 'Jhon',
-            age: '31'
-      }
-      const response= await agent.patch('https://httpbin.org/patch').send(query);
+  it('Consume PATCH service ', async () => {
+    const query = {
+      name: 'Jhon',
+      age: '31'
+    };
+    const response = await agent.patch('https://httpbin.org/patch').send(query);
 
-      expect(response.status).to.equal(statusCode.OK);
-      expect(response.body.json).to.eql(query);
-      expect(response.body.files).to.eql({});
-    });
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.json).to.eql(query);
+    expect(response.body.files).to.eql({});
+  });
 
-    it('Consume DELETE service ', async () => {
-        const query= {
-            name:'Hector',
-            age: 21,
-            city: 'Medellin'
-        }
-        const response= await agent
-            .delete('https://httpbin.org/delete')
-            .send(query);
+  it('Consume DELETE service ', async () => {
+    const query = {
+      name: 'Hector',
+      age: 21,
+      city: 'Medellin'
+    };
+    const response = await agent
+      .delete('https://httpbin.org/delete')
+      .send(query);
 
-        expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.json).to.eql(query);
-    });
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.json).to.eql(query);
+  });
 
-    describe('Consume POST service', async () => {
-        const query ={
-            name: 'Hector',
-            age: 21,
-            city: 'Medellin'
-        }
-        const response= await agent
-            .post('https://httpbin.org/post')
-            .send(query);
-        
-        expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.json).to.eql(query);
-    });
+  describe('Consume POST service', async () => {
+    const query = {
+      name: 'Hector',
+      age: 21,
+      city: 'Medellin'
+    };
+    const response = await agent
+      .post('https://httpbin.org/post')
+      .send(query);
 
-    describe('Consume PUT service ', async () => {
-        const query = {
-            name: 'Jhonatan',
-            age: 24,
-            city: 'Medellin'
-        }
-        const response = await agent
-            .put('https://httpbin.org/put')
-            .send(query);
-        expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.json).to.eql(query);
-    });
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.json).to.eql(query);
+  });
+
+  describe('Consume PUT service ', async () => {
+    const query = {
+      name: 'Jhonatan',
+      age: 24,
+      city: 'Medellin'
+    };
+    const response = await agent
+      .put('https://httpbin.org/put')
+      .send(query);
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.json).to.eql(query);
+  });
 });
